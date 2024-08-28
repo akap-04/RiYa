25f3e073cd3a59bbbde536aa9fe83b251cacd320
akap-04 <134932009+akap-04@users.noreply.github.com>
2024-08-28 11:47:40 +0530

    Time: 0 ms (100%), Space: 7.7 MB (35.3%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -4,13 +4,10 @@ public:
 
     int n = start ^ goal;
     int cnt = 0;
-    
-    
     while (n) {
         cnt += n & 1;  // Add the least significant bit to cnt
         n >>= 1;       // Right shift n to process the next bit
     }
-
     return cnt;
     }
 };
