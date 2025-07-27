e17751743b386b0c5ec07086ba4a54d99f689d54
akap-04 <134932009+akap-04@users.noreply.github.com>
2025-07-27 18:22:48 +0530

    Time: 27 ms (55.39%), Space: 27.6 MB (28.41%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -4,8 +4,8 @@ public:
         int n=text1.size(),m=text2.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
 
-        for(int j=0;j<m;j++)dp[0][j]=0;
-        for(int i=0;i<n;i++)dp[i][0]=0;
+        for(int j=0;j<=m;j++)dp[0][j]=0;
+        for(int i=0;i<=n;i++)dp[i][0]=0;
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
