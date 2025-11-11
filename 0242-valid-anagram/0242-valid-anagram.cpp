89003516e52041ad04d172dcc8f04206768147f8
akap-04 <134932009+akap-04@users.noreply.github.com>
2025-11-11 19:56:47 +0530

    Time: 1 ms (59.93%), Space: 9.9 MB (11.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()) return false;
-
-        std::sort(s.begin(),s.end());
-        std::sort(t.begin(),t.end());
+        if(s.length()!=t.length())return false;
+        unordered_map<char,int>mp;
         
-        if(s==t){
-            return true;
+        for(char c:s)mp[c]++;
+        for(char c:t){
+            if(mp[c]==0)return false;
+            mp[c]--;
         }
-        return false;
+        return true;
     }
 };
-//do frequency method as well for better tc
